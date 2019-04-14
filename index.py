99161cce31381b164f9f93b62c8be1da9bf3da63
Rohit Sarkar <rohitsarkar5398@gmail.com>
2019-04-14 12:48:19 +0530

    integrate model

--- index.py
+++ index.py
@@ -1,5 +1,10 @@
 from flask import Flask, request
+import pickle
+import numpy as np
+
 app = Flask(__name__)
+
+model = pickle.load(open("modelNN.pkl","rb"))
 
 @app.route("/")
 def hello():
@@ -7,12 +12,15 @@ def hello():
 
 @app.route("/predict", methods=['POST'])
 def predict():
-    if validInput(request.form):
+    features = request.get_json()['features']
+    if validInput(features):
         # TODO: Call the machine learning model and return the appropriate data
-        return "The input is valid"
+        ans = model.predict(features)
+        print(ans)
+        return np.array2string(ans)
     return "Invalid form input."
 
     
-def validInput(form):
+def validInput(features):
     # TODO: Validate the form inputs here
     return True
