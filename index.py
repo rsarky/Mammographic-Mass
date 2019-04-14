f0646213b43f6abd1e554d595fdd3cf12478b5c6
Rohit Sarkar <rohitsarkar5398@gmail.com>
2019-04-14 19:40:40 +0530

    API is functional

--- index.py
+++ index.py
@@ -1,9 +1,11 @@
 from flask import Flask, request
+from flask_cors import CORS
+from flask import jsonify
 import pickle
-import numpy as np
+import json
 
 app = Flask(__name__)
-
+CORS(app)
 model = pickle.load(open("modelNN.pkl","rb"))
 
 @app.route("/")
@@ -12,15 +14,27 @@ def hello():
 
 @app.route("/predict", methods=['POST'])
 def predict():
-    features = request.get_json()['features']
-    if validInput(features):
-        # TODO: Call the machine learning model and return the appropriate data
-        ans = model.predict(features)
-        print(ans)
-        return np.array2string(ans)
-    return "Invalid form input."
+    rawFeatures = request.get_json()['features']
+    features = parse(rawFeatures[0])
+    ans = model.predict_proba([features])
+    ansList = ans.tolist()
+    print(ansList)
+    return json.dumps({ 'probabilities': ansList })
 
     
 def validInput(features):
     # TODO: Validate the form inputs here
     return True
+def parse(ip):
+    features = []
+    # Append BIRADS and shape as is
+    features.append(ip[0])
+    features.append(ip[1])
+    arrShape = [ 0 for i in range(4) ]
+    arrShape[ip[2]] = 1
+    arrMargin = [ 0 for i in range(5) ]
+    arrMargin[ip[3]] = 1
+    features = features + arrShape + arrMargin
+    features.append(ip[4])
+    return features
+    
